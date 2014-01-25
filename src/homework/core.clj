07c30370e316781e4051ff20c9aa084ab48815e4
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-01-25 12:35:34 -0600

    nothing to see here

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,21 +1,32 @@
 (ns homework.core)
 
 (defn find-node [predicate tree]
-    (if (predicate tree)
-      tree
-      (if tree
-        (first (map #(find-node predicate %) (:children tree))))))
+  (if (predicate tree)
+    tree
+    (first (filter #(find-node predicate %) (:children tree)))))
 
 (defn find-all [predicate tree]
   #{})
 
 (defn find-node-custom [predicate get-children tree]
-  ;you will probably say something like:
-  ;(let [children (get-children tree)])
-  [])
+  (if (predicate tree)
+    tree
+    (some
+      #(find-node-custom predicate get-children %)
+      (get-children tree))))
 
 
-(defn find-node-with-path [path-so-far path-predicate get-children tree])
+(defn find-node-with-path [path-so-far path-predicate get-children tree]
+  (let [new-path (conj path-so-far tree)]
+    (if (path-predicate new-path)
+    tree
+    (some
+      #(find-node-with-path
+         new-path
+         path-predicate
+         get-children
+         %)
+      (get-children tree)))))
 ;CONTRACT
 ;if it's in the subree
   ;return it
