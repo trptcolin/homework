6b45c76c9e1da5c1762fc7b957ab6fbb206375a9
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-01-26 15:06:22 -0600

    dunked on it.  boom

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,9 +1,9 @@
 (ns homework.core)
 
+(declare find-node-custom)
+
 (defn find-node [predicate tree]
-  (if (predicate tree)
-    tree
-    (first (filter #(find-node predicate %) (:children tree)))))
+  (find-node-custom predicate #(:children %) tree))
 
 (defn find-all [predicate tree]
   (let [new-finds (if (predicate tree)
@@ -22,25 +22,10 @@
       (get-children tree))))
 
 
-(defn find-node-with-path [path-so-far path-predicate get-children tree]
-  (let [new-path (conj path-so-far tree)]
-    (if (path-predicate new-path)
-    tree
-    (some
-      #(find-node-with-path
-         new-path
-         path-predicate
-         get-children
-         %)
-      (get-children tree)))))
-;CONTRACT
-;if it's in the subree
-  ;return it
-;if it's not in the subtee
-  ;return nil
-
-;IDEA:
-; if predicate tree
-;  tree
-;  else
-; get the children of the tree and iterate through them, checking each one
+(defn find-node-with-path [_ path-predicate get-children tree]
+  (let [get-children-metagraph (fn [node]
+                                 (vec (map
+                                        (fn [child] {:node child :path (conj (vec (:path node)) child)})
+                                        (get-children (:node node)))))
+        metagraph-predicate #(path-predicate (:path %))]
+    (:node (find-node-custom metagraph-predicate get-children-metagraph {:node tree :path [tree]}))))
