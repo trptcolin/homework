eaf9e2ef912d46ff40912c36438e27a496e8b2f1
Colin Jones <trptcolin@gmail.com>
2014-01-30 21:28:02 -0600

    Make things work

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,29 +1,31 @@
 (ns homework.core)
 
-(defn find-node [predicate tree]
-    (if (predicate tree)
-      tree
-      (if tree
-        (first (map #(find-node predicate %) (:children tree))))))
+(defn find-all-seq [get-children predicate tree]
+  (filter
+    #(and (predicate %) %)
+    (tree-seq get-children get-children tree)))
 
 (defn find-all [predicate tree]
-  #{})
+  (set (find-all-seq :children predicate tree)))
 
 (defn find-node-custom [predicate get-children tree]
-  ;you will probably say something like:
-  ;(let [children (get-children tree)])
-  [])
+  (first (find-all-seq get-children predicate tree)))
 
+(defn find-node [predicate tree]
+  (find-node-custom predicate :children tree))
 
-(defn find-node-with-path [path-so-far path-predicate get-children tree])
-;CONTRACT
-;if it's in the subree
-  ;return it
-;if it's not in the subtee
-  ;return nil
+(defn add-path [loc]
+  (fn [child] {:value child
+               :ppath (conj (:ppath loc) child)}))
 
-;IDEA:
-; if predicate tree
-;  tree
-;  else
-; get the children of the tree and iterate through them, checking each one
+;; A path seq would be more pleasant than this opaque path-predicate.
+(defn find-node-with-path [_ path-predicate get-children tree]
+  (let [root-node {:value tree :ppath [tree]}
+        children-values (comp get-children :value)
+        get-children-with-path (fn [loc]
+                                 (map (add-path loc)
+                                      (children-values loc)))]
+    (:value (find-node-custom (comp path-predicate :ppath)
+                              get-children-with-path
+                              root-node))))
+
